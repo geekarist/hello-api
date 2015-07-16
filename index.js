ab94d5b25b058e86eed479d733391a81d6065a03
geekarist <christophe.pele@gmail.com>
2015-07-16 08:18:29 +0200

    Try to secure /sayhellotome endpoint

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello!');
-});
-
-app.get('/sayhellotome', function (req, res) {
-    res.send('Hello, you!');
-});
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var users = [
     {login: 'cpele', password: 'secret'},
     {login: 'otheruser', password: 'secret2'}
 ];
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new LocalStrategy(
+        {session: false},
+        function (username, password, done) {
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].login === username) {
+                    if (password === users[i].password) {
+                        return done(null, users[i]);
+                    } else {
+                        return done(null, false);
+                    }
+                }
+            }
+            return done(null, false);
+        }));
+
+app.get('/', function (req, res) {
+    res.send('Hello!');
+});
+
+app.get('/sayhellotome',
+    passport.authenticate('local', {failureRedirect: '/'}),
+    function (req, res) {
+        res.send('Hello, %s!', req.user);
+    });
 
 var server = app.listen(3000, function () {
     var port = server.address().port;
