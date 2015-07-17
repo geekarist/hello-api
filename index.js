5918464837d66c4894b1f2bee11507ca21416b91
geekarist <christophe.pele@gmail.com>
2015-07-18 01:17:02 +0200

    Get session login right
    It was not working because the secured endpoint and
    the authentication endpoint were the same.
    
    Now there are 3 endpoints:
    
    - /login for authentication
    - /logout
    - / is the secured endpoint

--- index.js
+++ index.js
@@ -4,40 +4,44 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var expressSession = require('express-session');
 
+app.use(expressSession({secret: 'keyboard cat', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// User repository
 var users = [
     {username: 'cpele', password: 'secret'},
     {username: 'otheruser', password: 'secret2'}
 ];
 
-// TODO body parser? app.router?
-app.use(expressSession({secret: 'keyboard cat', resave: true, saveUninitialized: true}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(
-    new LocalStrategy(
-        function (username, password, done) {
-            console.log('Checking %s: %s', username, password);
-            for (var i = 0; i < users.length; i++) {
-                if (users[i].username === username) {
-                    if (password === users[i].password) {
-                        console.log('Password Ok for %s: %s', username, password);
-                        return done(null, users[i]);
-                    } else {
-                        console.log('Wrong password for %s: %s', username, password);
-                        return done(null, false);
-                    }
-                }
+// Find user by name and launch done callback
+var findUserByName = function (username, password, done) {
+    console.log('Checking %s: %s', username, password);
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].username === username) {
+            if (password === users[i].password) {
+                console.log('Password Ok for %s: %s', username, password);
+                return done(null, users[i]);
+            } else {
+                console.log('Wrong password for %s: %s', username, password);
+                return done(null, false);
             }
-            console.log('User not found: %s', JSON.stringify(username));
-            return done(null, false);
-        }));
+        }
+    }
+    console.log('User not found: %s', JSON.stringify(username));
+    return done(null, false);
+};
 
+// Use local strategy to authenticate user by name
+passport.use(new LocalStrategy(findUserByName));
+
+// Serialize user into session when authenticating
 passport.serializeUser(function (user, done) {
     console.log('Serializing %s', JSON.stringify(user));
     done(null, user.username);
 });
 
+// Deserialize user from session
 passport.deserializeUser(function (username, done) {
     var matchingUsers = users.filter(function (item) {
         return item.username === username;
@@ -47,38 +51,34 @@ passport.deserializeUser(function (username, done) {
     done(null, u);
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello!');
+// Check if user is authenticated or send Unauthorized status
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.sendStatus(401);
+}
+
+// Say hello to user if she is authenticated
+app.get('/', ensureAuthenticated, function (req, res) {
+    res.send('Hello ' + req.user.username + '!');
 });
 
-app.get('/sayhellotome', function (req, res, next) {
-    passport.authenticate('local', function (err, user) {
-        if (err) {
-            console.log('Error');
-            return next(err);
-        }
-        if (!user) {
-            console.log('No user:', user);
-            return res.sendStatus(401);
-        }
-        req.logIn(user, function (err) {
-            if (err) {
-                console.log('Error in logIn');
-                return next(err);
-            }
-            console.log('Auth Ok');
-            return res.send('Hello, ' + req.user.username + '!');
-        })
-    })(req, res, next);
-});
+// User authentication endpoint
+app.get('/login',
+    passport.authenticate('local', {failureRedirect: '/'}),
+    function (req, res) {
+        res.send('Ok ' + req.user.username + ', you\'re in!');
+    });
 
+// User logout endpoint
 app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
 
+// Launch server
 var server = app.listen(3000, function () {
     var port = server.address().port;
-
     console.log('Hello API listening at port %s', port);
 });
